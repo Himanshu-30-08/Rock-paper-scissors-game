d2aa6d1ca0cdebc2fcf8374b706437487776e3ed
Himanshu-30-08 <sharmahimanshu2089@gmail.com>
2025-08-24 10:19:49 +0530

    Added Comp and human choices

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+function getcomputerchoice() {
+    rand=Math.floor(Math.random() * 3);
+    if (rand === 0) {
+        return "rock";
+    } else if (rand === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function gethumanchoice() {
+    let choice = prompt("Enter rock, paper or scissors: ").toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        alert("Invalid choice. Please try again.");
+        return gethumanchoice();
+    }
+}
